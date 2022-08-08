6d9e3bb4e4c374014bd7e0416e4dc2dc77d86eac
Nutifafa Afi Attor <attorfafa@gmail.com>
2022-08-08 07:08:29 +0000

    created first layer skeleton of application

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.*;
+import java.lang.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("""
+                Hello, welcome to the Algorithm Tester!
+                =======================================
+                
+                What type of algorithm would you like to implement
+                ( 1 ) Search Algorithm
+                ( 2 ) Sorting Algorithm
+                
+                """);
+        int algoOption = sc.nextInt();
+
+        if (algoOption == 1){
+            System.out.println("Search Algorithmssssss");
+        }
+        else if (algoOption == 2) {
+            System.out.println("Sorting Algorithmssssss");
+        }
+        else {
+            System.out.println("Enter a valid input");
+        }
     }
 }
